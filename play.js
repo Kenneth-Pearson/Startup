6b24289c903554e756833ebd1ba4782de834ee56
Kenneth-Pearson <156155448+Kenneth-Pearson@users.noreply.github.com>
2024-02-23 14:38:00 -0700

    timer() experimentation

--- play.js
+++ play.js
@@ -16,3 +16,24 @@ function logout() {
   localStorage.setItem("username", "Login_To_Track_Your_Score");
   window.location.href = "play.html";
 }
+
+function timer(seconds) {
+  if (document.getElementById("start_Button").getAttribute("disabled") === "true") 
+  {
+    return;
+  }
+  let i = seconds;
+  document.getElementById("start_Button").setAttribute("disabled", true);
+
+  const intervalDuration = 1000; // in milliseconds (1 second in this case)
+
+  const intervalTimer = setInterval(() => {
+    document.getElementById("seconds_remaining").innerText = "Time Remaining: " + i;
+      i--;
+      if (i < 0) {
+        document.getElementById("seconds_remaining").innerText = "Time Remaining: -";
+        document.getElementById("start_Button").removeAttribute("disabled");
+        clearInterval(intervalTimer); // Stop the timer when countdown reaches 0
+      }
+  }, intervalDuration);
+}
