e1ebf7f0c8467d18f13d3c33174a4a7bb6759d05
Kenneth-Pearson <156155448+Kenneth-Pearson@users.noreply.github.com>
2024-02-22 19:44:36 -0700

    First scores.js experimentation

--- scores.js
+++ scores.js
@@ -1,1 +1,19 @@
 // This is scores.js
+
+// new score is added into the end of the nested array with 11 positions [[Max, 34], [Brooklyn, 32], ... , [Just_Played_Score, 33]]
+// sort the array by numerical value
+// display content 
+localStorage.setItem("first_name", "3");
+var leaderboard = new Array(11);
+Array[0] = [first_name, first_score];
+Array[1] = [second_name, second_score];
+Array[2] = [third_name, third_score];
+Array[3] = [fourth_name, fourth_score];
+Array[4] = [fifth_name, fifth_score];
+Array[5] = [sixth_name, sixth_score];
+Array[6] = [seventh_name, seventh_score];
+Array[7] = [eighth_name, eighth_score];
+Array[8] = [ninth_name, ninth_score];
+Array[9] = [tenth_name, tenth_score];
+Array[10] = [new_name, new_score];
+
