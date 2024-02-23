c6fff60504a2bcfe9edc7977165274e9efcd1642
Kenneth-Pearson <156155448+Kenneth-Pearson@users.noreply.github.com>
2024-02-22 20:10:44 -0700

    further scores.jss experimentation

--- scores.js
+++ scores.js
@@ -3,17 +3,46 @@
 // new score is added into the end of the nested array with 11 positions [[Max, 34], [Brooklyn, 32], ... , [Just_Played_Score, 33]]
 // sort the array by numerical value
 // display content 
-localStorage.setItem("first_name", "3");
+// localStorage.setItem("first_name", "3");
+// var storedValue = localStorage.getItem("first_name");
+// document.getElementById("first_name").textContent = storedValue;
+
+//var element = document.getElementById("exampleElement");
+//var integerValue = parseInt(element.textContent, 10);
+
 var leaderboard = new Array(11);
-Array[0] = [first_name, first_score];
-Array[1] = [second_name, second_score];
-Array[2] = [third_name, third_score];
-Array[3] = [fourth_name, fourth_score];
-Array[4] = [fifth_name, fifth_score];
-Array[5] = [sixth_name, sixth_score];
-Array[6] = [seventh_name, seventh_score];
-Array[7] = [eighth_name, eighth_score];
-Array[8] = [ninth_name, ninth_score];
-Array[9] = [tenth_name, tenth_score];
-Array[10] = [new_name, new_score];
+function create_display()
+{
+leaderboard[0] = [first_name, first_score];
+leaderboard[1] = [second_name, second_score];
+leaderboard[2] = [third_name, third_score];
+leaderboard[3] = [fourth_name, fourth_score];
+leaderboard[4] = [fifth_name, fifth_score];
+leaderboard[5] = [sixth_name, sixth_score];
+leaderboard[6] = [seventh_name, seventh_score];
+leaderboard[7] = [eighth_name, eighth_score];
+leaderboard[8] = [ninth_name, ninth_score];
+leaderboard[9] = [tenth_name, tenth_score];
+leaderboard[10] = ["Testing", "50"];
+};
+
+update_display()
+{
+    leaderboard.sort(function(a,b){
+        return a[1]-b[1];
+    });
+    //first_update
+    localStorage.setItem("first_name", leaderboard[0][0]);
+    var storedValue = localStorage.getItem("first_name");
+    document.getElementById("first_name").textContent = storedValue;
+    localStorage.setItem("first_name", leaderboard[0][1]);
+    var storedValue = localStorage.getItem("first_score");
+    document.getElementById("first_name").textContent = storedValue;
+    
+};
+
+update_display();
+
+
+console.log(leaderboard[0])
 
