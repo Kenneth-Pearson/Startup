1d8c7f71bb9947b617768d361bfcaf97e4547b72
Kenneth-Pearson <156155448+Kenneth-Pearson@users.noreply.github.com>
2024-03-11 17:01:38 -0600

    First Express Use

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-// This is index.js
-function login() {
-  const nameEl = document.querySelector("#name");
-  localStorage.setItem("username", nameEl.value);
-  window.location.href = "play.html";
-}
+const express = require("express");
+const app = express();
 
-function logout() {
-  const nameEl = document.querySelector("#name");
-  localStorage.setItem("username", "Login_To_Track_Your_Score");
-  window.location.href = "play.html";
-}
+// Creating your own middleware - logging
+app.use((req, res, next) => {
+  console.log(req.originalUrl);
+  next();
+});
+
+// Built in middleware - Static file hosting
+app.use(express.static("public"));
+
+// Routing middleware
+app.get("/store/:storeName", (req, res) => {
+  res.send({ name: req.params.storeName });
+});
+
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
+});
+
+// Listening to a network port
+const port = 4000;
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`);
+});
---
+++ public/index.js
@@ -0,0 +1,12 @@
+// This is index.js
+function login() {
+  const nameEl = document.querySelector("#name");
+  localStorage.setItem("username", nameEl.value);
+  window.location.href = "play.html";
+}
+
+function logout() {
+  const nameEl = document.querySelector("#name");
+  localStorage.setItem("username", "Login_To_Track_Your_Score");
+  window.location.href = "play.html";
+}
